ad75fee20547f3fa01056c21f48ffee416856253
Magda <supercoder@192.168.100.5>
2025-01-31 13:24:03 +0100

    type sours auskommentiert für einen test

--- src/news-types.ts
+++ src/news-types.ts
@@ -4,13 +4,13 @@ type NewsApiResponse ={
   articles: Article[];
 }
 
-type Source = {
-  id: string | null;
-  name: string;
-}
+// type Source = {
+//   id: string | null;
+//   name: string;
+// }
 
 type Article = {
-  source: Source;
+  // source: Source;
   author: string | null;
   title: string;
   description: string;
@@ -20,5 +20,5 @@ type Article = {
   content: string;
 }
   
-export type { NewsApiResponse, Source, Article };
+export type { NewsApiResponse, Article };
   
