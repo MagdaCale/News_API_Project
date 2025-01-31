e2fbe2120ec0bef04d89cd5738bf867cb43818d3
bebzbzbz <balzerbeatrice@gmail.com>
2025-01-31 14:25:41 +0100

    fetchData

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,19 @@
-const APIKEY = import.meta.env.VITE_NEWS_API_KEY;
+import ky from "ky";
+import { NewsApiResponse } from "./news-types";
+
+const APIKEY = import.meta.env.VITE_PRIVATE_KEY;
 console.log(APIKEY);
+
+async function getFilteredArticles(keyword: string, lang: string, sortby: string) {
+    try {
+        const allArticles = await ky.get<NewsApiResponse>(`https://newsapi.org/v2/everything?q=${keyword}&language=${lang}&sortBy=${sortby}&apiKey=${APIKEY}`).json();
+        console.log(allArticles.articles);
+        return allArticles.articles;
+    } catch (error) {
+        console.log("An error occurred: " + error);
+        return [];
+    }
+}
+
+// getFilteredArticles("money", "de", "relevance")
+console.log(getFilteredArticles("money", "de", "relevance"));
